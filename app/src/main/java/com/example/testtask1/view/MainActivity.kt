318dc2f5243494b833976e2424f9a1bce762805b
SergeiBaz <125749715+SergeiBaz@users.noreply.github.com>
2023-12-01 12:55:01 +0300

    add navigate fragment

--- app/src/main/java/com/example/testtask1/view/MainActivity.kt
+++ app/src/main/java/com/example/testtask1/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testtask1
+package com.example.testtask1.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testtask1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
