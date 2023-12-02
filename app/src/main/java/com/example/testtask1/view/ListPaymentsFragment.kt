a5eda065461df3f7481ea471622edb6534978a20
SergeiBaz <125749715+SergeiBaz@users.noreply.github.com>
2023-12-03 00:22:00 +0300

    add logout

--- app/src/main/java/com/example/testtask1/view/ListPaymentsFragment.kt
+++ app/src/main/java/com/example/testtask1/view/ListPaymentsFragment.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.testtask1.R
 import com.example.testtask1.databinding.FragmentListPaymentsBinding
+import com.example.testtask1.model.TokenStorage
 import com.example.testtask1.view.adapters.PaymentAdapter
 import com.example.testtask1.viewModel.AuthViewModel
 import com.example.testtask1.viewModel.PaymentViewModel
@@ -19,6 +26,14 @@ class ListPaymentsFragment : Fragment() {
     private lateinit var binding: FragmentListPaymentsBinding
     private lateinit var adapter: PaymentAdapter
     private val viewModelPayment by viewModels<PaymentViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            backPressed()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +44,9 @@ class ListPaymentsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.backButton.setOnClickListener {
+            backPressed()
+        }
         adapter = PaymentAdapter()
         viewModelPayment.getListPayments()
         viewModelPayment.currentPaymentState.observe(viewLifecycleOwner) {
@@ -39,4 +57,8 @@ class ListPaymentsFragment : Fragment() {
         binding.paymentRecyclerView.adapter = adapter
     }
 
+    private fun backPressed() {
+        TokenStorage.token = ""
+        findNavController().navigate(R.id.logInFragment)
+    }
 }
